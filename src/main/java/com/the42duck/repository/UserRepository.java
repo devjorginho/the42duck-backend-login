b50b89e3be333bdee293325410d223a6ec18cc59
devjorginho <jcarvalho.sweb@gmail.com>
2025-10-07 07:00:12 +0100

    t

---
+++ src/main/java/com/the42duck/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.the42duck.repository;
+
+import com.the42duck.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
