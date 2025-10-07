d90ab00c90fbc43fdae3e4a2edda070b5bf0a093
devjorginho <jcarvalho.sweb@gmail.com>
2025-10-07 08:07:45 +0100

    t

--- src/main/java/com/the42duck/the42duckbackend/SecurityConfig.java
+++ src/main/java/com/the42duck/the42duckbackend/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.the42duck.the42duckbackend;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,5 +16,11 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())          // desativa CSRF
             .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // permite todas as rotas sem login
         return http.build();
+    }
+
+    // ADICIONE ESTE BEAN
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 }
