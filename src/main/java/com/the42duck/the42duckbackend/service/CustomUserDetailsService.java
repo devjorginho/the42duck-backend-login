cc5e80df000eaf921c4a321c75eb976819173ad0
devjorginho <jcarvalho.sweb@gmail.com>
2025-10-07 07:34:03 +0100

    t

--- src/main/java/com/the42duck/the42duckbackend/service/CustomUserDetailsService.java
+++ src/main/java/com/the42duck/the42duckbackend/service/CustomUserDetailsService.java
@@ -1,30 +1,30 @@
-package com.the42duck.the42duckbackend.service;
+//package com.the42duck.the42duckbackend.service;
 
-import com.the42duck.the42duckbackend.model.User;
-import com.the42duck.the42duckbackend.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-import org.springframework.security.core.userdetails.User.UserBuilder;
-import org.springframework.security.core.userdetails.User as SpringUser;
+//import com.the42duck.the42duckbackend.model.User;
+//import com.the42duck.the42duckbackend.repository.UserRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//import org.springframework.security.core.userdetails.User.UserBuilder;
+//import org.springframework.security.core.userdetails.User as SpringUser;
 
-@Service
-public class CustomUserDetailsService implements UserDetailsService {
+//@Service
+//public class CustomUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
+//    @Autowired
+//    private UserRepository userRepository;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username)
-                        .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + username));
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        User user = userRepository.findByUsername(username)
+//                        .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + username));
 
-        UserBuilder builder = SpringUser.withUsername(user.getUsername());
-        builder.password(user.getPassword()); // senha já codificada em BCrypt
-        builder.roles("USER"); // você pode personalizar depois
+//        UserBuilder builder = SpringUser.withUsername(user.getUsername());
+//        builder.password(user.getPassword()); // senha já codificada em BCrypt
+//        builder.roles("USER"); // você pode personalizar depois
 
-        return builder.build();
-    }
-}
+//        return builder.build();
+//    }
+//}
