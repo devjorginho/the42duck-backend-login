67bed46260b5b52826835cd35ae91a23e8814a71
devjorginho <jcarvalho.sweb@gmail.com>
2025-10-07 06:40:34 +0100

    t

---
+++ src/main/java/com/the42duck/the42duckbackend/HelloController.java
@@ -0,0 +1,13 @@
+package com.the42duck.the42duckbackend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, 42 Duck!";
+    }
+}
---
+++ src/main/java/com/the42duck/the42duckbackend/The42DuckBackendApplication.java
@@ -0,0 +1,11 @@
+package com.the42duck.the42duckbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class The42DuckBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(The42DuckBackendApplication.class, args);
+    }
+}
